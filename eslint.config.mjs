3158477b565200ec9f91b08253893ade0aa55382
Eric Mancebo <ericmancebo6950@gmail.com>
2025-01-11 00:34:40 +0100

    , - m

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
