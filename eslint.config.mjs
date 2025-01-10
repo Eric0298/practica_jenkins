b792b9b224cb55f1fe319bddd79d4c4da7c2372d
Eric Mancebo <ericmancebo6950@gmail.com>
2025-01-11 00:40:17 +0100

    esperemos que sea el ultimo commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,19 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: {globals: globals.browser}},
-  ...tseslint.configs.recommended,
-  pluginReact.configs.recommended,
   {
-    settings: {
-      react: {
-        version: "detect"
-      }
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact, // Configuración como objeto
     },
     rules: {
-      "react/react-in-jsx-scope": "off"
-    }
-  }
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  ...tseslint.configs.recommended,
 ];
