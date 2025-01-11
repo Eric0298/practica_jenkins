5ed721f99d165ea8e6d3f636e43bb2480710f66a
Eric Mancebo <ericmancebo6950@gmail.com>
2025-01-11 12:36:38 +0100

    otro commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
 import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from '@eslint/compat';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.browser } },
   {
     plugins: {
-      react: pluginReact
+      react: pluginReact,
+      ts: tseslint
     },
     settings: {
       react: {
@@ -18,7 +20,10 @@ export default [
     },
     rules: {
       'react/react-in-jsx-scope': 'off'
-    }
-  },
-  ...tseslint.configs
-];
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended'
+    ]
+  }
+]);
